bbe90de239c2a9e7c348fd4521054fe24d0ead48
Leandro Andriani <andriani.leandro25@gmail.com>
2023-11-06 12:01:48 -0300

    Pomodoro ready!!

--- App.js
+++ App.js
@@ -22,9 +22,7 @@ export default function App() {
   const [currentState, setCurrentState] = useState(0 | 1 | 2);
   const [clock, setClock] = useState(false);
   // estado long para saber si el break es long o short, para iterar entre ambos
-  const [long, setLong] = useState(false);
-  // estado para marcar cuantos tiempos de trabajo se harán
-  const [working, setWorking] = useState(0);
+  const [short, setShort] = useState(true);
 
   useEffect(() => {
     let interval = null;
@@ -37,55 +35,31 @@ export default function App() {
       clearInterval(interval);
     }
 
-    if (time === 0 && currentState === 0) {
+    if (currentState === 3) {
+      setCurrentState(0);
+    }
+    if (time === 0 && currentState === 0 && short === true) {
       setCurrentState(1);
       setTime(300);
-      if (time === 0 && currentState === 1) {
-        setCurrentState(0);
-        setTime(1500);
-      }
-      if (time === 0 && currentState === 2) {
-        setCurrentState(0);
-        setTime(1500);
-      } else {
-        setTime(900);
-      }
+      setShort(false);
     }
-
+    if (time === 0 && currentState === 0 && short === false) {
+      setCurrentState(2);
+      setTime(900);
+      setShort(true);
+    }
+    if (time === 0 && currentState === 1) {
+      setCurrentState(0);
+      setTime(1500);
+    }
+    if (time === 0 && currentState === 2) {
+      setCurrentState(0);
+      setTime(1500);
+      setShort(true);
+    }
+    console.log(time, currentState);
     return () => clearInterval(interval);
-  }, [clock, time]);
-
-  /*
-
-  Respaldo del useEffect
-
-   useEffect(() => {
-    let interval = null;
-
-    if (clock) {
-      interval = setInterval(() => {
-        setTime(time - 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
-
-    if (time === 0) {
-      setClock(false);
-      if (currentState === 0) {
-        setTime(1500);
-      }
-      if (currentState === 1) {
-        setTime(300);
-      } else {
-        setTime(900);
-      }
-    }
-
-    return () => clearInterval(interval);
-  }, [clock, time]);
-
-  */
+  }, [clock, time, currentState]);
 
   function handleStartStop() {
     clock ? playStopSound() : playStartSound();
